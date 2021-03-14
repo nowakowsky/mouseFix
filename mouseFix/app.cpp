afe53700bbf63890e943ffedd756210a0a9f161a
nowakowsky <patryk@nowakowski.pw>
2021-03-14 18:47:32 +0100

    first working version

--- mouseFix/app.cpp
+++ mouseFix/app.cpp
@@ -1,29 +1,87 @@
 #include <iostream>
 #include <Windows.h>
 
+// cooldown in milliseconds
+#define COOLDOWN 200
+
 class MouseHook{
 private:
 	MouseHook() {}
 public:
-	DOUBLE lastCheck;
+	SYSTEMTIME lastCheck;
 	static MouseHook& instance(){
 		static MouseHook mouseHook;
 		return mouseHook;
 	}
 	
 };
+HHOOK hMouseHook;
+SYSTEMTIME systemTime;
+MouseHook mouseHook = MouseHook::instance();
+
+FILETIME addTime(FILETIME ft)
+{
+	ULARGE_INTEGER tmp;
+
+	tmp.LowPart = ft.dwLowDateTime;
+	tmp.HighPart = ft.dwHighDateTime;
+
+	// convert cooldownTime to milliseconds
+	tmp.QuadPart += (COOLDOWN * 10000);
+	ft.dwLowDateTime = tmp.LowPart;
+	ft.dwHighDateTime = tmp.HighPart;
+
+	return ft;
+}
+
+
+bool shouldBlock(SYSTEMTIME lastTime, SYSTEMTIME currentTime)
+{
+	// conver SYSTEMTIME TO FILETIME
+	FILETIME flastTime;
+	FILETIME fcurrentTime;
+	SystemTimeToFileTime(&lastTime, &flastTime);
+	SystemTimeToFileTime(&currentTime, &fcurrentTime);
+	
+	// add cooldown time
+	flastTime = addTime(flastTime);
+
+	auto test = CompareFileTime(&flastTime, &fcurrentTime);
+	if (test == -1 || test == 0) return false;
+	return true;
+}
+
+LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode < 0) return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+	if (wParam == WM_LBUTTONDOWN) {
+		GetSystemTime(&systemTime);
+
+		// block
+		if (shouldBlock(mouseHook.lastCheck, systemTime)) return CallNextHookEx(hMouseHook, -1, wParam, lParam);
+		
+		// allow and update lastCheck
+		std::cout << "allow" << std::endl;
+
+		mouseHook.lastCheck = systemTime;
+		return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+	}
+	return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+};
+
 
 
 int main() {
-	MouseHook mouseHook = MouseHook::instance();
-	SYSTEMTIME systemTime;
+	
+	MSG msg;
+	HINSTANCE hInstance = GetModuleHandle(NULL);
 	GetSystemTime(&systemTime);
-	SystemTimeToVariantTime(&systemTime, &mouseHook.lastCheck);
+	mouseHook.lastCheck = systemTime;
 
-
+	hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, hInstance, NULL);
 	while (true) {
-		std::cout << "ok";
-		break;
+		GetMessage(&msg, 0, 0, 0);
+		DispatchMessage(&msg);
 	}
 	return 0;
 };
